7e5ce761f196103a9e05bc2a18e0f56e1702b35f
Iris The Fox <iris-miz@yandex.ru>
2019-04-08 15:24:15 +0300

    add authenticate verification to group

--- app/controllers/flrs_controller.rb
+++ app/controllers/flrs_controller.rb
@@ -1,9 +1,10 @@
 class FlrsController < ApplicationController
-  before_action :set_flr, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
   before_action do 
     redirect_to new_user_session_path unless current_user && current_user.admin?
   end
+  before_action :set_flr, only: [:show, :edit, :update, :destroy]
+
 
 
   # GET /flrs
