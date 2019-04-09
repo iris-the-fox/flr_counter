3e104f61180630def5be2126b4e9fbf87f83b00f
Iris The Fox <iris-miz@yandex.ru>
2019-04-09 12:14:01 +0300

    change group uniqueness scope

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,11 @@
 class Group < ApplicationRecord
 	validates :number, :color, :name, presence: true
-	validates :number, :color, :name, uniqueness: true
+	validates :name, uniqueness: { scope: :flr_id,
+    message: ": Группа с таким названием уже существует" }
+    validates :number, uniqueness: { scope: :flr_id,
+    message: ": Группа с таким номером уже существует" }
+    validates :color, uniqueness: { scope: :flr_id,
+    message: ": Группа с таким цветом уже существует" }
 	belongs_to :flr, dependent: :destroy
 	belongs_to :user, dependent: :destroy
 	has_many :stories
