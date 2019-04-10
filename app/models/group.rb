b9a5ecc8fb3690039467ebbddf3c2c9666ecbd97
Iris The Fox <iris-miz@yandex.ru>
2019-04-10 12:10:22 +0300

    add stories to group form

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,4 +10,15 @@ class Group < ApplicationRecord
 	belongs_to :user, dependent: :destroy
 	has_many :stories
 
+	def all_stories
+      self.stories.map(&:name).join("\r\n\r\n")
+    end
+
+    def all_stories=(names)
+      self.stories = names.split("\r\n\r\n").map do |name|
+        self.stories.where(name: name.strip).first_or_create!
+      end
+    end
+
+
 end
