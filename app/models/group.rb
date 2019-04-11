c8caa663622206783bf443155c74b95fcd31529e
Iris The Fox <iris-miz@yandex.ru>
2019-04-11 13:20:28 +0300

    change all_stories method

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,25 +1,29 @@
 class Group < ApplicationRecord
 	validates :number, :color, :name, presence: true
 	validates :name, uniqueness: { scope: :flr_id,
-    message: ": Группа с таким названием уже существует" }
-  validates :number, uniqueness: { scope: :flr_id,
-    message: ": Группа с таким номером уже существует" }
-  validates :color, uniqueness: { scope: :flr_id,
-    message: ": Группа с таким цветом уже существует" }
+      message: ": Группа с таким названием уже существует" }
+    validates :number, uniqueness: { scope: :flr_id,
+      message: ": Группа с таким номером уже существует" }
+    validates :color, uniqueness: { scope: :flr_id,
+      message: ": Группа с таким цветом уже существует" }
 	belongs_to :flr
 	belongs_to :user
 	has_many :stories, dependent: :destroy
-  default_scope {order :number}
+    default_scope {order :number}
 
 	def all_stories
-    self.stories.map(&:name).join("\r\n\r\n")
-  end
-
-  def all_stories=(names)
-    self.stories = names.split("\r\n\r\n").map do |name|
-      Story.create!(name: name.strip, group_id: self.id)
+      self.stories.map(&:name).join("\r\n\r\n")
     end
-  end
+
+    def all_stories=(names)
+      self.stories = []
+        names.split("\r\n\r\n").map do |name|
+      	if Story.find_by(name: name.strip).nil?
+      	  story = Story.create!(name: name.strip)
+          self.stories << story
+        end
+      end
+    end
 
 
 end
