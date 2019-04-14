02ffd635b0fdbc6abe8f8bf81c4d9913980ef8df
Iris The Fox <iris-miz@yandex.ru>
2019-04-14 18:33:55 +0300

    add before save to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :story
+  before_save :add_link_and_author
+
+  private
+    def add_link_and_author
+    	msg = Review_finder.new(self.text)
+    	self.link = msg.link
+    	self.author = msg.author
+    end
 end
