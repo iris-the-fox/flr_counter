07160816600c51658653363374ec6a0acbfc40e5
Iris The Fox <iris-miz@yandex.ru>
2019-04-15 19:41:21 +0300

    fix gsub

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :story
   before_save :add_link_and_author, if: :new_record?
-  belongs_to :group
+  belongs_to :group, optional: true
 
   private
     def add_link_and_author
-    	text = self.text.gsub( /\n/, "" ).gsub( /\r/, "" )
+    	text = self.text.gsub( /\n/, "" ).gsub( /\r/, "" ).gsub(" ", "" )
     	msg = Review_finder.new(text)
     	self.link = msg.link
     	self.author = msg.author
--- app/services/message_ws.rb
+++ app/services/message_ws.rb
@@ -2,7 +2,7 @@ class MessageWS
   attr_accessor :body, :link, :author
 
   def initialize(row)
-    @body = row.xpath('td/table/tr/td/table/tr/td/div/font').text.gsub( /\n/, "" ) # текст
+    @body = row.xpath('td/table/tr/td/table/tr/td/div/font').text.gsub( /\n/, "" ).gsub( /\r/, "" ).gsub( " ", "" ) # текст
     @author =  row.xpath('td/span/a/b').text # ник
     @link = row.xpath('td/table/tr/td/span/a[1]/@href').text.gsub( /\"/, "" ) #ссылка
   end
