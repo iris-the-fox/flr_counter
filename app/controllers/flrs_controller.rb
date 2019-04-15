f452b510608ebb3804b5dbba6c44b8486aad6992
Iris The Fox <iris-miz@yandex.ru>
2019-04-15 13:46:10 +0300

    add reviews to txt

--- app/controllers/flrs_controller.rb
+++ app/controllers/flrs_controller.rb
@@ -83,6 +83,6 @@ class FlrsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def flr_params
-      params.require(:flr).permit(:number, :start_date, :info, :link, :current, :first_page)
+      params.require(:flr).permit(:number, :start_date, :info, :link, :current, :first_page, :last_page)
     end
 end
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,8 +1,4 @@
 module ReviewsHelper
-
-
-
-
-
+	
 
 end
