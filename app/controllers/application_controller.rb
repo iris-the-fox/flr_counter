262088d2f98046a4c3ed4dfe5873816f39c3bb73
Iris The Fox <iris-miz@yandex.ru>
2019-04-18 10:44:36 +0300

    add my_group

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :set_current_flr
+  before_action :set_my_group
 
   def set_current_flr
     @current_flr = Flr.find_by(current: true)
   end
+
+  def set_my_group
+  	@my_group = @current_flr.groups.find_by(user_id: current_user.id)
+  end
+
 end
