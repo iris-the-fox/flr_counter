4574f6c002d7462d0d06bfd0c0c742d855b73d62
Iris The Fox <iris-miz@yandex.ru>
2019-04-18 10:49:08 +0300

    change validations for stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,8 +1,8 @@
 class Story < ApplicationRecord
   belongs_to :group, optional: true
-  default_scope {order :name}
+  default_scope {order :created_at}
   validates :name, presence: true
-  validates :name, uniqueness: {
+  validates :name, uniqueness: { scope: :group_id,
     message: ": Рассказ с таким названием уже существует" }
   has_many :reviews, dependent: :destroy
   accepts_nested_attributes_for :reviews, reject_if: :all_blank
