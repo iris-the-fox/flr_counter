8d48cd4764ba3572818057feffb3516a6332af59
iris <iris-miz@yandex.ru>
2019-05-29 04:06:43 -0700

    change default reviews scope

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@
 class Review < ApplicationRecord
   include StopList
   belongs_to :story
-  default_scope { order :created_at }
+  default_scope { order created_at: :desc }
   before_save :add_link_and_author, if: :new_record?
   belongs_to :group
   has_one :message
@@ -12,14 +12,15 @@ class Review < ApplicationRecord
 
   def add_link_and_author
     if text.present?
-    text = delete_smiles(self.text)
-    text = clean_text(text)
-    puts text
-    puts '==='
-    msg = Review_finder.new(text)
-    self.link = msg.link
-    self.author = msg.author
-    self.message = msg.msg
+      text = delete_smiles(self.text)
+      text = clean_text(text)
+      puts text
+      puts '==='
+      msg = Review_finder.new(text)
+      self.link = msg.link
+      self.author = msg.author
+      self.message = msg.msg
+    end
   end
-  end
+  
 end
